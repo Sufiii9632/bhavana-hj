5ac8b9878947026124d13262375eb0875433346a
Sufiii9632 <158042726+Sufiii9632@users.noreply.github.com>
2024-02-23 11:51:39 +0530

    uiyuy

---
+++ Basic data structures.py
@@ -0,0 +1,12 @@
+print("list")
+l1=[1,2,"ABC",3,"xyz",2.3]
+print(l1)
+print("dictionary")
+d1={"a":134,"b":266,"c":343}
+print(d1)
+print("tuple")
+t1=(10,20,30,40,50,60)
+print(t1)
+print("sets")
+s1={10,30,40,50,10,30,40,50,20,50,50}
+print(s1)
